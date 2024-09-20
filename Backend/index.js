0a9906be951e78855517c696a2369b6db5efe05f
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-09-20 20:19:34 +0530

    init

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 4000
+const HOST = process.env.HOST || 'localhost'
+const MONGO_URL = process.env.MONGO_URL
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Server [FaceGram]'
+  })
+})
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("DB Connected")
+    app.listen(PORT, () => console.log(`URL: http://${HOST}:${PORT}/api`))
+  })
+  .catch(() => console.log('DB NOT connected', e))
---
+++ Frontend/src/components/ThemeButton.jsx
@@ -0,0 +1,23 @@
+import { PropTypes } from "prop-types"
+import { MdOutlineWbSunny } from "react-icons/md"
+import { RiMoonClearLine } from "react-icons/ri"
+
+const ThemeButton = ({ isDarkMode, toggle }) => {
+  return (
+    <button
+      className="w-10 h-10 a-center right-5 bottom-5 absolute rounded-md ring-2 ring-sky-600 dark:ring-sky-500 cursor-pointer"
+      onClick={toggle}
+    >
+      <span className="text-slate-800 dark:text-slate-200">
+        {isDarkMode ? <MdOutlineWbSunny size={20} /> : <RiMoonClearLine size={20} />}
+      </span>
+    </button>
+  )
+}
+
+ThemeButton.propTypes = {
+  isDarkMode: PropTypes.bool,
+  toggle: PropTypes.any,
+}
+
+export default ThemeButton
