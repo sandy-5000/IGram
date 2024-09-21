3660a1995ac9ac72c8140b515e6a8ecd89976c78
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-09-21 09:44:32 +0530

    added link

--- Frontend/src/components/Logo.jsx
+++ Frontend/src/components/Logo.jsx
@@ -1,14 +1,15 @@
 import { PropTypes } from "prop-types"
 import logoDark from "/src/assets/Logo-Dark.png"
+import { Link } from "react-router-dom"
 
 const Logo = ({ size = 90 }) => {
   return (
-    <div>
+    <Link to="/">
       <div style={{ width: size }} className="hover-effect relative a-center cursor-pointer">
         <img style={{ width: size }} className="z-[2] absolute left-0 right-0" src={logoDark} />
         <img style={{ width: size }} className="animate-pulse z-[2] absolute left-0 right-0" src={logoDark} />
       </div>
-    </div>
+    </Link>
   )
 }
 
