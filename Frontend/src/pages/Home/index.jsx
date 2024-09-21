3885dbe99146938a7ab4b0dd498fbfdf9447ff21
Vamsi-Krishna4737 <157821700+Vamsi-Krishna4737@users.noreply.github.com>
2024-09-21 13:28:36 +0530

    feed

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 
 import Feed from "../Feeds/index.jsx"
-=======
->>>>>>> aacaea3c302ae47541018e12ee1eec9aaef274b2
 import MainLayout from "/src/layouts/MainLayout"
 
 const Home = () => {
@@ -11,13 +8,10 @@ const Home = () => {
       <div className="h-screen w-screen a-center">
         <p className="text-gray-800 dark:text-gray-200">Home!</p>
       </div>
-<<<<<<< HEAD
       <div>
         <Feed/>
       </div>
 
-=======
->>>>>>> aacaea3c302ae47541018e12ee1eec9aaef274b2
     </MainLayout>
   )
 }
