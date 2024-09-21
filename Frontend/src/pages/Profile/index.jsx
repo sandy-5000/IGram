da3dc06db2e16f1e818a75b466d2ffc07c372ebe
Vamsi-Krishna4737 <157821700+Vamsi-Krishna4737@users.noreply.github.com>
2024-09-21 12:52:13 +0530

    profile

--- Frontend/src/pages/Profile/index.jsx
+++ Frontend/src/pages/Profile/index.jsx
@@ -464,7 +464,8 @@ function ProfilePage() {
               </div>
 
               <div className="mb-4">
-                <label className="block text-gray-700 font-bold mb-2 dark:text-[#2563eb]">Confirm Password</label>
+                <label className="block text-gray-700 font-bold mb-2 dark:text-[#2563eb]
+                ">Confirm Password</label>
                 <Field
                   name="confirmPassword"
                   type="password"
